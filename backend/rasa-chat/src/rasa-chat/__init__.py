0bcb006a99ecdf15eaa80d000bed55c5fd7f0f46
Jonathan <jonanfu@gmail.com>
2025-10-31 10:54:10 -0500

    Delete backend directory

--- backend/rasa-chat/src/rasa-chat/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Cargar variables desde el archivo .env
-load_dotenv()
-
-# Opcional: debug, imprimir si quieres verificar
-print("Twilio SID cargado:", os.getenv("TWILIO_ACCOUNT_SID"))
